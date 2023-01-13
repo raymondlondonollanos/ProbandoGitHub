7ab928c943bd89fc22656eb0dad1f88d54e3ede1
ACER <ACER@EMILIO>
2023-01-12 20:43:48 -0500

    Add project files.

---
+++ ProbandoGitHub/main.cpp
@@ -0,0 +1,30 @@
+//***************************************************************************
+// Plantilla Proyecto Vacio C++ v1.0 (12/01/2023)
+//***************************************************************************
+//
+// General   >  Dir Salida       > $(SolutionDir)\Bin
+//			 >  Dir Intermedio   > $(SolutionDir)\Tmp
+//			 >  Nombre Destino	 > $(ProjectName)         (Release / 64)
+//			 >  Nombre Destino   > $(ProjectName)_d       (Debug / 64)
+//			 >  Nombre Destino   > $(ProjectName)_32      (Release / 32)
+//			 >  Nombre Destino   > $(ProjectName)_32d     (Debug / 32)
+//     
+// Avanzado  >  Juego de caract  > Multibyte
+// C/C++     >  General			 > Comp multiproc        > Si
+//			 >  Optimizacion     > Tamaño o velocidad    > Codigo radido
+//			 >  Gen de codigo    > Bib en tiempo de ej   > MT (Release)
+//                               > Bib en tiempo de ej   > MTd (Debug)
+//                               > Modelo de punt f1     > Rapido
+//           >  Idioma           > Estandar del leng     > Ultimo borrador
+// 
+//***************************************************************************
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hola mundo!! \n";
+	std::cin.get();
+
+	return 0;
+}
